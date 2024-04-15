efcf9f7fff9b6791e693010b12c0d7659217f788
Thang <nguyenvietthangthk@gmail.com>
2024-04-15 19:40:26 +0700

    'src'

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log("Hello NodeJs");
+// console.log("Hello NodeJs");
+import {sumTwoNum , printConsole } from "./util.js";
+// import subTwoNum from "./util.js";
+import {default as subTwoNum} from "./util.js";
+
+function main(){
+    printConsole("tong 2 so 1va 2 la " + sumTwoNum(2,3));
+    printConsole("hieu 2 so 1va 2 la " + subTwoNum(7,3));
+}
+
+main();
